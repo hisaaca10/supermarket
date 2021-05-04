4149acfb355066b6f5c72a97d14f6ac3b8b6ca16
Consware <cwadmin@CWLAPTOP-18.consware.com.co>
2021-05-04 11:29:36 -0500

    s

--- hasslerprueba/Controllers/ClientesController.cs
+++ hasslerprueba/Controllers/ClientesController.cs
@@ -4,42 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using PruebaMichelNiebles.DB_Context;
-using PruebaMichelNiebles.Models;
+using hasslerprueba.Models;
 
-namespace PruebaMichelNiebles.Controllers
+namespace hasslerprueba.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class ClientesController : ControllerBase
     {
-        private readonly ApplicationDbContext _context;
+    
+   
 
-        public ClientesController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
-
-        // GET: api/Clientes
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Cliente>>> GetClientes()
-        {
-            return await _context.Clientes.ToListAsync();
-        }
-
-        // GET: api/Clientes/5
+ 
         [HttpGet("{id}")]
         public async Task<ActionResult<Cliente>> GetCliente(int id)
         {
-            var cliente = await _context.Clientes.FindAsync(id);
+         
 
             if (cliente == null)
             {
                 return NotFound();
             }
 
-            return cliente;
         }
 
         // PUT: api/Clientes/5
@@ -51,24 +37,7 @@ namespace PruebaMichelNiebles.Controllers
                 return BadRequest();
             }
 
-            _context.Entry(cliente).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ClienteExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
+         
             return NoContent();
         }
 
@@ -76,15 +45,14 @@ namespace PruebaMichelNiebles.Controllers
         [HttpPost]
         public async Task<ActionResult<Cliente>> PostCliente(Cliente cliente)
         {
-            _context.Clientes.Add(cliente);
-            await _context.SaveChangesAsync();
+      
 
-            return CreatedAtAction("GetCliente", new { id = cliente.IdCliente }, cliente);
+         
         }
 
         private bool ClienteExists(int id)
         {
-            return _context.Clientes.Any(e => e.IdCliente == id);
+            return false;
         }
     }
 }
--- hasslerprueba/Models/Cliente.cs
+++ hasslerprueba/Models/Cliente.cs
@@ -4,19 +4,16 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace PruebaMichelNiebles.Models
+namespace hasslerprueba.Models
 {
     public class Cliente
     {
         [Key]
         public int IdCliente { get; set; }
-        public string PrimerNombre { get; set; }
-        public string SegundoNombre { get; set; }
-        public string PrimerApellido { get; set; }
-        public string SegundoApellido { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
         public int Edad { get; set; }
         public string Direccion { get; set; }
-        public Int64 Telefono { get; set; }
         public Int64 Celular { get; set; }
     }
 }
